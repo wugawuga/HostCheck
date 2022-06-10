09468e486108666d2122bb5c3a603a37a54463a1
Wugawuga <dnr3655@naver.com>
2022-06-10 23:04:41 +0900

    create project

---
+++ src/test/java/com/wuga/host/Check/CheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuga.host.Check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
