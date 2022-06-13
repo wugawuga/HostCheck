331ed1ff9fec337e00577bcbfea1d0c51715a233
Wugawuga <dnr3655@naver.com>
2022-06-13 14:15:55 +0900

    edit - CheckController

--- src/main/java/com/wuga/host/check/controller/CheckController.java
+++ src/main/java/com/wuga/host/check/controller/CheckController.java
@@ -3,28 +3,33 @@ package com.wuga.host.check.controller;
 
 import com.wuga.host.check.domain.HostsDTO;
 import com.wuga.host.check.service.HostService;
-import java.io.IOException;
 import java.net.URI;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CheckController {
 
     private final HostService hostServiceImpl;
+    private final HttpSession session;
 
-    @GetMapping("/hosts")
+    @GetMapping("/api/hosts")
     public ResponseEntity<?> hosts() {
 
         List<HostsDTO> all = hostServiceImpl.findAll();
@@ -32,21 +37,25 @@ public class CheckController {
         result.put("data", all);
         result.put("count", all.size());
 
+        log.info("session count = {}", session.getAttribute("count"));
+
         return ResponseEntity.ok().body(result);
     }
 
-    @PostMapping("/hosts")
-    public ResponseEntity<?> createHost() throws IOException {
+    @PostMapping("/api/hosts")
+    public ResponseEntity<?> saveHost(@RequestParam("hostName") String hostName)
+        throws RuntimeException, UnknownHostException {
 
-        HostsDTO result = hostServiceImpl.save();
+        session.setAttribute("count", hostServiceImpl.count() + 1);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-            .build()
-            .toUri();
+        HostsDTO result = hostServiceImpl.save(hostName);
+
+        URI location = creatLocation();
+
         return ResponseEntity.created(location).body(result);
     }
 
-    @GetMapping("/hosts/{ip}")
+    @GetMapping("/api/hosts/{ip}")
     public ResponseEntity<?> hostByIp(@PathVariable String ip) {
 
         HostsDTO result = hostServiceImpl.findByIp(ip);
@@ -54,32 +63,28 @@ public class CheckController {
         return ResponseEntity.ok().body(result);
     }
 
-    @PatchMapping("/hosts/{ip}")
+    @PutMapping("/api/hosts/{ip}")
     public ResponseEntity<?> updateHost(@PathVariable String ip, HostsDTO hostsDTO) {
 
-        HostsDTO update = hostServiceImpl.update(ip, hostsDTO);
+        HostsDTO result = hostServiceImpl.update(ip, hostsDTO);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-            .build()
-            .toUri();
-        return ResponseEntity.created(location).body(update);
+        URI location = creatLocation();
+
+        return ResponseEntity.created(location).body(result);
     }
 
-    @DeleteMapping("/hosts/{ip}")
+    @DeleteMapping("/api/hosts/{ip}")
     public ResponseEntity<?> deleteHost(@PathVariable String ip) {
 
         hostServiceImpl.deleteByIp(ip);
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-            .build()
-            .toUri();
-
-        return ResponseEntity.ok().body(location);
+        return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/hosts/health/{ip}")
-    public ResponseEntity<> checkHealth(@PathVariable String ip) {
-
-        hostServiceImpl.
+    private URI creatLocation() {
+        return ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .build()
+            .toUri();
     }
 }
