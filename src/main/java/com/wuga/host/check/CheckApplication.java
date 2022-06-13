dec9b76ce2d413045973f26a6e4b02e9d7b200cb
Wugawuga <dnr3655@naver.com>
2022-06-13 14:16:27 +0900

    edit - settings

--- src/main/java/com/wuga/host/check/CheckApplication.java
+++ src/main/java/com/wuga/host/check/CheckApplication.java
@@ -2,8 +2,10 @@ package com.wuga.host.check;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CheckApplication {
 
 	public static void main(String[] args) {
